e4aa22fea81d42c9e5483de72cdd1ec9608dd698
funlt <leotoa@163.com>
2021-06-28 15:26:26 +0800

    init project

---
+++ src/main/java/com/funlt/freehandwrite/FreeHandWriteApplication.java
@@ -0,0 +1,13 @@
+package com.funlt.freehandwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeHandWriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeHandWriteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/funlt/freehandwrite/api/TestController.java
@@ -0,0 +1,14 @@
+package com.funlt.freehandwrite.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @RequestMapping("/handwrite")
+    public String test(){
+        return "游离笔迹";
+    }
+}
